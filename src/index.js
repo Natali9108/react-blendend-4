e6b73312f6ff144d1d11ef4692e4f177bc42343c
Danil Takiylin <danadanil900@gmail.com>
2023-06-10 15:27:03 +0200

    addpersist

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
+import { PersistGate } from 'redux-persist/integration/react';
 import 'modern-normalize';
 
 import { App } from 'components';
 import { GlobalStyles, theme } from 'styles';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { persistor, store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Global styles={GlobalStyles} />
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { todoReducer } from './todoSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'todos',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todoReducer);
 
 export const store = configureStore({
   reducer: {
-    todos: todoReducer,
+    todos: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
