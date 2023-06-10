72225ccbf902c72ebcb3c93b7e2ec1728d33591d
Andrii <sushkoandy1@gmail.com>
2023-06-10 15:36:23 +0300

    addReduxSlice

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,6 +12,7 @@ import {
 } from 'components';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { TodoItem } from 'components/TodoItem/TodoItem';
 
 export const App = () => {
   const [todos, setTodos] = useState(
@@ -49,20 +50,7 @@ export const App = () => {
           {todos.length === 0 && (
             <Text textAlign="center">There are no any todos ... </Text>
           )}
-
-          <Grid>
-            {todos.length > 0 &&
-              todos.map((todo, index) => (
-                <GridItem key={todo.id}>
-                  <Todo
-                    id={todo.id}
-                    text={todo.text}
-                    counter={index + 1}
-                    onClick={deleteTodo}
-                  />
-                </GridItem>
-              ))}
-          </Grid>
+          <TodoItem />
         </Container>
       </Section>
     </>
